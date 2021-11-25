317bf7945670edf8250fb4aaa8b329c49ca05f6c
olivenbarcelon <olivenbarcelon@gmail.com>
2021-11-25 11:10:19 +0800

    add logs

--- project/src/main/java/semicolon/umjavaws/UmJavaWsApplication.java
+++ project/src/main/java/semicolon/umjavaws/UmJavaWsApplication.java
@@ -2,12 +2,16 @@ package semicolon.umjavaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
+@Log4j2
 public class UmJavaWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmJavaWsApplication.class, args);
+		log.info("Sring Version: " + SpringVersion.getVersion());
 	}
-
 }
