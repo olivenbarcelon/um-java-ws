15b0afcfe59fb98748a84694d0192a0a486aabc7
olivenbarcelon <olivenbarcelon@gmail.com>
2021-11-26 03:52:24 +0800

    update git workflow

--- project/src/test/java/semicolon/umjavaws/UmJavaWsApplicationTests.java
+++ project/src/test/java/semicolon/umjavaws/UmJavaWsApplicationTests.java
@@ -1,13 +1,19 @@
 package semicolon.umjavaws;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import semicolon.umjavaws.info.Info;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UmJavaWsApplicationTests {
-
+	@Autowired
+	private WebTestClient webTestClient;
+	
 	@Test
 	void contextLoads() {
+		webTestClient.get().uri("/info").exchange().expectStatus().isOk().expectBody(Info.class);
 	}
-
 }
--- project/src/test/java/semicolon/umjavaws/info/InfoTest.java
+++ project/src/test/java/semicolon/umjavaws/info/InfoTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class InfoTest {
-    @Autowired
+	@Autowired
 	private WebTestClient webTestClient;
 	
 	@Test
