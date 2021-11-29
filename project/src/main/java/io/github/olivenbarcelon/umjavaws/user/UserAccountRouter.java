80f86d96f93cd7a4383bccadd9c65551b5034c72
olivenbarcelon <olivenbarcelon@gmail.com>
2021-11-29 19:59:22 +0800

    add create user account

--- project/src/main/java/io/github/olivenbarcelon/umjavaws/user/UserAccountRouter.java
+++ project/src/main/java/io/github/olivenbarcelon/umjavaws/user/UserAccountRouter.java
@@ -14,6 +14,7 @@ public class UserAccountRouter {
     @Bean
     public RouterFunction<ServerResponse> routeUserAccount(UserAccountHandler handler) {
         return RouterFunctions.route(RequestPredicates.POST("/api/user-account")
-            .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::store);
+            .and(RequestPredicates.accept(MediaType.APPLICATION_JSON))
+            .and(RequestPredicates.contentType(MediaType.APPLICATION_JSON)), handler::store);
     }
 }
