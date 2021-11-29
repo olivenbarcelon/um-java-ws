f259d78dfad21e7d21a8e0b79bf1f4a39f1f142d
olivenbarcelon <olivenbarcelon@gmail.com>
2021-11-29 20:51:38 +0800

    add create user account

--- project/src/test/java/io/github/olivenbarcelon/umjavaws/user/UserAccountTest.java
+++ project/src/test/java/io/github/olivenbarcelon/umjavaws/user/UserAccountTest.java
@@ -48,10 +48,7 @@ public class UserAccountTest {
             .accept(MediaType.APPLICATION_JSON)
             .body(Mono.just(entity), UserAccountEntity.class)
             .exchange()
-            .expectStatus().is4xxClientError()
-            .expectHeader().contentType(MediaType.APPLICATION_JSON)
-            .expectBody()
-            .jsonPath("$.data.uuid").isNotEmpty();
+            .expectStatus().is4xxClientError();
         // Store user
         entity.setRole(Role.USER.toString());
         webTestClient.post().uri("/api/user-account")
