149cd731d4c147324013e16b41a8f45e0aa26d0e
olivenbarcelon <olivenbarcelon@gmail.com>
2022-01-12 15:46:22 +0800

    fix create user account

--- project/src/main/java/io/github/olivenbarcelon/umjavaws/user/UserAccountEntity.java
+++ project/src/main/java/io/github/olivenbarcelon/umjavaws/user/UserAccountEntity.java
@@ -2,10 +2,6 @@ package io.github.olivenbarcelon.umjavaws.user;
 
 import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -18,24 +14,16 @@ import lombok.Setter;
  *  [TLS version incompatibility when using latest JDKs with MySQL <8 #182] - https://github.com/mirromutth/r2dbc-mysql/issues/182
  */
 @Table("user_account")
+@Getter
 @Setter
 public class UserAccountEntity {
     @Id
-    //@JsonIgnoreProperties(allowGetters = false, allowSetters = true)
     private long id;
-    @Getter
     private String uuid;
-    @Getter
     private String username;
-    @Getter
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    @Getter
     private String role;
-    @Getter
     private LocalDateTime createdAt;
-    @Getter
     private LocalDateTime updatedAt;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private LocalDateTime deletedAt;
 }
